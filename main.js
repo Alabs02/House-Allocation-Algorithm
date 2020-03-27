8aabcc87949d35ba5755295ec1c7c0bc09058632
Alabs <alabson.inc@gmail.com>
2020-03-27 22:37:08 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function calHouse() {
+    let n = document.getElementById("numOfHouses").value;
+    let i = document.getElementById("numOfI").value;
+    let p = document.getElementById("prize").value;
+    let b = parseInt(document.getElementById("budget").value, 10);
+
+    console.log(n, i, p, b); 
+    // convert p into an array and sort
+    let array = p.split(",");
+    array.sort();
+    console.log(array)
+
+    //convert elements of array to intergers
+    for (let i = 0; i < array.length; i++)  array[i] = parseInt(array[i], 10);  
+    console.log(array);
+
+    // value of A which is the summation of all the cost of houses
+    let sum = array.reduce(function(a, b) {
+        return a + b;
+    }, 0);
+    console.log(`cost of all houses: ${sum}`);
+     
+    function buyHouse() {
+        for (let i = 0; i <= array.length && b >= array[i]; i++ && b-array[i]) {
+            if (i >= 0) {
+                let inc = i + 1;
+                console.log(inc);
+                // document.getElementById("result").innerHTML = (`You can purchase ${inc} houses`);
+            } 
+        }
+    }
+    buyHouse();
+}
