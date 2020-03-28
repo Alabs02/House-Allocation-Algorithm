be16302a791528003380496c57c5ccc2f41c4136
Alabs <alabson.inc@gmail.com>
2020-03-28 23:16:04 +0100

    Fixed Bugs in main.js

---
+++ README.md
@@ -0,0 +1,26 @@
+# House-Allocation-Algorithm
+Google KickStart Round A allocation problem
+Problem
+
+There are N houses for sale. The i-th house costs Ai dollars to buy. You have a budget of B dollars to spend.
+What is the maximum number of houses you can buy?
+
+Solution
+Step 1
+There are N number of Houses
+Step2
+There are A dollar to buy houses
+step3
+Budget B dollar to spend
+
+To get the maximum amount of houses you can buy, the user must input the total number of houses, the individual prices of these house separated by space or comma
+and budgeted amount
+
+Next split the prices and convert string to an array and convert prices to integers. Afterwards
+Sort the elelments in the array in an increasing order.
+
+Write a loop to check if the lowest is is equal to or greater than B the budget.
+Finally subtract the individual prices from the budget until it is not possible
+Boom you get the maximum number of houses can be purchased base on the budgeted amount.
+
+Thank You
--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function calHouse() {
     let p = document.getElementById("prize").value;
     let b = parseInt(document.getElementById("budget").value, 10);
 
-    console.log(n, i, p, b); 
+    console.log(n, i, p, b);
     // convert p into an array and sort
     let array = p.split(",");
     array.sort();
     console.log(array)
 
     //convert elements of array to intergers
-    for (let i = 0; i < array.length; i++)  array[i] = parseInt(array[i], 10);  
+    for (let i = 0; i < array.length; i++)  array[i] = parseInt(array[i], 10);
     console.log(array);
 
     // value of A which is the summation of all the cost of houses
@@ -19,17 +19,28 @@ function calHouse() {
         return a + b;
     }, 0);
     console.log(`cost of all houses: ${sum}`);
-     
-    function buyHouse() {
-        for (let i = 0; i <= array.length && b >= array[i]; b-array[i] && i++) {
-            if (i >= 0) {
-		num = b - array[i]
-                let inc = i + 1;
-                console.log(`You can purchase ${inc} houses`);
-		console.log(num);
-                document.getElementById("result").innerHTML = (`You can purchase ${inc} houses`);
-            } 
+
+    function maxHouses() {
+      let prevBought = array[0];
+      let houses = prevBought;
+      for (let i = 0; i <= array.length && b >= array[i]; i++) {
+        spend = b;
+        let x = Math.min(spend - array[i]);
+        prevBought = x;
+        if (i >= 0 && prevBought >= array[i]) {
+          alert(x)
+          let counter = i + 1;
+          console.log(counter);
+          document.getElementById('result').innerText = (`You can purchase ${counter} houses`)
+          houses += x;
+          prevBought = x;
+          console.log(prevBought);
         }
+        else if(1 == array.length && b < array[i] ) {
+          let counter = i - i;
+          console.log(`You can purchase ${counter} houses!`);
+        }
+      }
     }
-    buyHouse();
+    maxHouses();
 }
